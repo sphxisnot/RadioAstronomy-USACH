dc18278be071dfe594f9d8d25d5ceaaae48d0b88
Reed <sphxisnot@duck.com>
2026-06-03 09:23:07 -0400

    Created main.py, with a rudimental, incomplete implementation.

---
+++ bin2fil/main.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+# por ahora haré que el filename sea escrito por el usuario, considerar implementar un walk quizá
+bin_file = input("Escribir nombre del archivo .bin a convertir:\n")
+# sanitizar input si es que se conserva esto (improbable si es que pasamos a C)
+# añadir inferencia de tipo más adelante?
+print("Elija una SDR:\n[1] RTL (8-bit unsigned int)\n[2] Airspy (16-bit signed int)")
+sdr_type = int(input("Ponga 1 o 2:"))
+
+
+# definición de funciones, hecho por modularidad y porque nos hará la vida un poco más facil si es que pasamos a C
+# en particular, definí esta porque hay que hacer el proceso muchas veces.
+def bin2cpow(data, off=0, d_type=1, channels=32):
+    """
+    Función para convertir el archivo de formato binario a poder de canal. Usando la Transformada de Fourier
+    Rápida (FFT).
+
+    Args:
+        data (str o pathObject): Nombre o path del archivo binario conteniendo la señal.
+        off (int): Desfase o posición en el archivo, necesario para los bucles.
+        d_type (int): 1 o 2. Tipo de dato contenido en el archivo binario, uint8 por defecto.
+        channels (int): Cantidad de canales, idealmente una potencia de 2.
+    Returns:
+        channel_pow (array-like): Array conteniendo el poder por canal de un solo sample.
+    """
+    data_type = {1: np.uint8, 2: np.int16}  # tipo de dato dependiendo de la sdr
+    samples = np.fromfile(
+        data, dtype=data_type[d_type], sep="", count=channels * 2, offset=off
+    )  # lectura de los datos, el separador es "empty" solo por precaución para que asuma binario, no sé si es
+    # realmente necesario, quizá hacer offset un multiplicador a channel en vez de un número independiente
+    freq_data = np.fft.fft(samples)  # transformada de Fourier
+    channel_pow = freq_data.real**2 + freq_data.imag**2  # conversión a poder de canal
+    return channel_pow
+
+
+# hay que hacer un bucle que itere sobre los datos, no sé si un while con un contador o un for sobre el tamaño
+# del archivo. De todas maneras sería usando información del archivo usando os, no leyendo el elemento entero
+# (por el bien de la memoria y mi consciencia)
